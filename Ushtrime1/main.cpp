e08144c1fd120f0b96d4cf8303fe38057d662c42
RigaZubaku <riga.zubaku@student.uni-pr.edu>
2023-11-05 22:57:40 +0100

    Ushtrime 1

---
+++ Ushtrime1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main() {
+
+   //cout<< "Pershendetje!";
+    //cout<< "\nPo mesojme gjuhen programuese \"C++\".";
+
+
+//int a,b,c;
+
+//cout<< "shtypni vleren a:";
+//cin >> a;
+
+//cout << "shtypni vleren b:";
+//cin >> b;
+
+//cout << "shtypni vleren c:";
+//cin >> c;
+
+//cout << "keni shtypur vlerat:\n";
+
+//double a=3.2, b=6, c=1.3;
+//double mesatarja = (a+b+c)/3;
+//cout << mesatarja;
+
+
+
+//int a, b ;
+//cout << "jepni brinjen a:";
+//cin>>a;
+
+//cout<<"jepni brinjen b:";
+//cin >>b;
+
+
+//int perimetri = 2 *(a+b);
+//cout << "perimetri i drejtekendeshit:"<<perimetri << endl;
+
+//int syprina = a*b;
+//cout <<"syprina e drejtekendeshit:"<< syprina;
+
+
+return 0;
+}
+
