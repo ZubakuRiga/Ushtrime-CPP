b3ccebb9f91bdeda724f38635629534c12296c37
RigaZubaku <riga.zubaku@student.uni-pr.edu>
2023-11-05 22:57:48 +0100

    Ushtrime 2

---
+++ Ushtrime2/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+int main() {
+ // int a,b,c;
+   // cout << "jepni vleren e a:";
+   // cin >> a;
+
+
+    //cout << "jepni vleren e b:";
+    //cin >> b;
+
+      // c=a;
+       //a=b;
+       //b=c;
+
+       //cout << "numrat pas nderrimit : a=" << a << ",b=" << b << endl;
+
+//double x=3.123455;
+// cout << setprecision(3) << x << endl;
+
+
+
+
+
+
+
+//double vleraEuro, vleraDollar, rata;
+
+//cout << "jepni vleren ne euro:";
+//cin>>vleraEuro;
+
+//cout << "jepni vleren e kembimit:";
+//cin >> rata;
+
+//vleraDollar = vleraEuro * rata;
+//cout << "vlera ne dollar : $" << vleraDollar << endl;
+
+//double vleraFinale = vleraDollar - vleraDollar*0.02;
+
+//cout << "vlera pas zbritjes 2% eshte :" << vleraFinale << endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
